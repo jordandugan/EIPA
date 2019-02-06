413ea8feffdc15ffd59758522de5fd6e528aacee
Jordan Dugan <jordandugan@AD.DOE.CARLETON.CA>
2019-02-06 16:59:19 -0500

    Wave Equation Solution

---
+++ WaveEquation.m
@@ -0,0 +1,48 @@
+nx = 50;
+ny = 50;
+
+G = sparse(nx*ny);
+for i = 1:nx
+    for j = 1:ny
+        n = j +(i-1)*ny;
+        if i == 1 || i == nx || j == 1 || j == ny
+            G(n,n) = 1;
+        else
+            nxm = j + (i-2)*ny;
+            nxp = j + i*ny;
+            nym = j-1 + (i-1)*ny;
+            nyp = j+1 + (i-1)*ny;
+            
+            G(n,n) = -4;
+            G(n,nxm) = 1;
+            G(n,nxp) = 1;
+            G(n,nym) = 1;
+            G(n,nyp) = 1; 
+        
+        end
+    end
+end
+
+figure(1)
+spy(G)
+
+[E,D] = eigs(G,9,'SM');
+
+eigenvalues = diag(D);
+figure(2)
+plot(eigenvalues,'o')
+
+eigenfunction = zeros(nx,ny,9);
+for i = 1:nx
+    for j = 1:ny
+        n = j +(i-1)*ny;
+        eigenfunction(i,j,:) = E(n,:);
+    end
+end
+
+for i = 1:9
+    figure(i+2)
+    surf(eigenfunction(:,:,i))
+end
+
+
